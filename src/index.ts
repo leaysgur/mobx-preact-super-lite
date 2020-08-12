a01e9cadf01747815231765b41497f24ef395406
Yuji Sugiura <y.sugiura.0316@gmail.com>
2020-08-12 18:43:10 +0900

    Add exports

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,5 @@ export {
 } from "./use-observer";
 export { Observer } from "./observer-component";
 export { useForceUpdate } from "./utils";
+export { useAsObservableSource } from "./use-as-observable-source";
+export { useLocalStore } from "./use-local-store";
